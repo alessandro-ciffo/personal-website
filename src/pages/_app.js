492eb7aea3a0a9c03fad04ed9b8fcc511d184f14
Alessandro Ciffo <diogene.ciffo@gmail.com>
2024-11-06 15:04:01 +0100

    feat: vercel speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/navbar';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       <main className="max-w-[960px] mx-auto px-4">
         <Component {...pageProps} />
         <Analytics />
+        <SpeedInsights />
       </main>
     </>
   );
